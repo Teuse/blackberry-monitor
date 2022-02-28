15ea342c42d4202fc68c42c60e8ff85bf975f684
Mathias Roder <mathias.roder@daimler.com>
2022-02-28 08:54:47 +0100

    Update

--- air_quality/air_quality_metrics.py
+++ air_quality/air_quality_metrics.py
@@ -1,10 +1,28 @@
 #!/usr/bin/python3
+import board
+from busio import I2C
+import adafruit_bme680
 
-import random
+# Create library object using our Bus I2C port
+i2c = I2C(board.SCL, board.SDA)
+bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c, debug=False)
 
-temperature = round(random.uniform(10.0, 30.0), 2)
-pressure = round(random.uniform(800.0, 1100.0), 2)
-humidity = round(random.uniform(0.0, 1.0), 2)
-quality = round(random.uniform(100000.0, 200000.0), 2)
+# change this to match the location's pressure (hPa) at sea level
+#bme680.sea_level_pressure = 1013.25
 
-print("air_quality,dummy=true temperature=" + str(temperature) + ",pressure=" + str(pressure) + ",humidity=" + str(humidity) + ",quality=" + str(quality))
+# You will usually have to add an offset to account for the temperature of
+# the sensor. This is usually around 5 degrees but varies by use. Use a
+# separate temperature sensor to calibrate this one.
+#temperature_offset = -5
+temperature_offset = 0
+
+# print("Temperature: %0.1f C" % (bme680.temperature + temperature_offset))
+# print("Gas: %d ohm" % bme680.gas)
+# print("Humidity: %0.1f %%" % bme680.relative_humidity)
+# print("Pressure: %0.3f hPa" % bme680.pressure)
+# print("Altitude = %0.2f meters" % bme680.altitude)
+
+temp = bme680.temperature + temperature_offset
+
+print("air_quality,room=living_room temperature=%f,gas=%d,humidity=%f,pressure=%f,altitude=%f" % temp, bme680.gas, bme680.relative_humidity, bme680.pressure, bme680.altitude)
+
