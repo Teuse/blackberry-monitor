9b730a9f97683497fdfd7f8d6eb6c733aef54ccc
Mathias Roder <mathias.roder@daimler.com>
2022-02-28 14:05:50 +0100

    Add bsec

--- air_quality/air_quality_metrics.py
+++ air_quality/air_quality_metrics.py
@@ -8,7 +8,7 @@ i2c = I2C(board.SCL, board.SDA)
 bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c, debug=False)
 
 # change this to match the location's pressure (hPa) at sea level
-#bme680.sea_level_pressure = 1013.25
+bme680.sea_level_pressure = 1032
 
 # You will usually have to add an offset to account for the temperature of
 # the sensor. This is usually around 5 degrees but varies by use. Use a
