c4660abb8d49c377b560a762faa3b1ceb3df1ad1
Mathias Roder <m.roder@evomotiv.de>
2022-03-19 14:52:13 +0100

    add influxdb

--- air_quality/influx_client.cpp
+++
@@ -1,7 +0,0 @@
-#include "influxdb_client.h"
-
-std::string InfluxClient::getEnv(std::string key)
-{
-    char *val = getenv(key.c_str());
-    return val == NULL ? std::string("") : std::string(val);
-}
