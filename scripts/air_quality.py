e968dfed7c870c1bda1b0f8793bf8f7957793a66
Mathias Roder <m.roder@evomotiv.de>
2023-02-06 12:07:20 +0100

    add influx client to air_quality script

--- scripts/air_quality.py
+++ scripts/air_quality.py
@@ -2,7 +2,11 @@
 import board
 from busio import I2C
 import adafruit_bme680
-from . import mh_z19
+import mh_z19
+
+from dotenv import dotenv_values
+import influxdb_client
+from influxdb_client.client.write_api import SYNCHRONOUS
 
 # Create library object using our Bus I2C port
 i2c = I2C(board.SCL, board.SDA)
@@ -10,29 +14,46 @@ bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c, debug=False)
 
 # change this to match the location's pressure (hPa) at sea level
 bme680.sea_level_pressure = 1033
-
-# You will usually have to add an offset to account for the temperature of
-# the sensor. This is usually around 5 degrees but varies by use. Use a
-# separate temperature sensor to calibrate this one.
-#temperature_offset = -5
-temperature_offset = 0
-
-temperature = bme680.temperature + temperature_offset
-
-mhz19 = mh_z19.get_all()
+bme680_temperature_offset = 0
 
 print("### BME860 ###")
-print("Temperature: %0.1f C" % temperature)
+print("Temperature: %0.1f C" % (bme680.temperature + bme680_temperature_offset))
 print("Gas: %d ohm" % bme680.gas)
 print("Humidity: %0.1f %%" % bme680.relative_humidity)
 print("Pressure: %0.3f hPa" % bme680.pressure)
 print("Altitude = %0.2f meters" % bme680.altitude)
 print()
 
+mhz19 = mh_z19.read_all()
+
 print("### MH-Z19B ###")
 print(mh_z19.read_all())
-print("CO2 = %0.2f " % mhz19.co2)
-print("Temperature = %0.1f C" % mhz19.temperature)
-print("TT = %0.2f " % mhz19.tt)
-print("SS = %0.2f " % mhz19.ss)
-print("UhUl = %0.2f " % mhz19.uhul)
+print("CO2 = %0.2f " % mhz19['co2'])
+print("Temperature = %0.1f C" % mhz19['temperature'])
+print("TT = %0.2f " % mhz19['tt'])
+print("SS = %0.2f " % mhz19['ss'])
+print("UhUl = %0.2f " % mhz19['uhul'])
+
+config = dotenv_values("../.env")
+bucket = config['INFLUXDB_BUCKET']
+org = config['INFLUXDB_ORG']
+token = config['INFLUXDB_TOKEN']
+url="http://localhost:8086"
+
+client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
+write_api = client.write_api(write_options=SYNCHRONOUS)
+
+p = influxdb_client.Point("my_measurement") \
+      .tag("location", "Prague") \
+      .field("temperature", mhz19['temperature']) \
+      .field("humidity", bme680.relative_humidity)  \
+      .field("pressure", bme680.pressure)  \
+      .field("gas", bme680.gas)  \
+      .field("altitude", bme680.altitude)  \
+      .field("co2", mhz19['co2'])  \
+      .field("tt", mhz19['tt'])  \
+      .field("ss", mhz19['ss']) \
+      .field("uhul", mhz19['uhul']) 
+
+write_api.write(bucket=bucket, org=org, record=p)
+
