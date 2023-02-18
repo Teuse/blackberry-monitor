4fae7742bb957b9f0d583d134c90bab954b1b1d2
Mathias Roder <m.roder@evomotiv.de>
2023-02-18 15:29:55 +0100

    add led update indication

--- scripts/update_led.py
+++ scripts/update_led.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 import mh_z19
+import time
  
 PIN_RED = 37
 PIN_GREEN = 40
@@ -12,6 +13,17 @@ co2 = mhz19['co2']
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(PINS, GPIO.OUT, initial=GPIO.LOW)
+
+GPIO.output(PIN_GREEN, 1)
+time.sleep(1)
+GPIO.output(PIN_GREEN, 0)
+GPIO.output(PIN_YELLOW, 1)
+time.sleep(1)
+GPIO.output(PIN_YELLOW, 0)
+GPIO.output(PIN_RED, 1)
+time.sleep(1)
+GPIO.output(PIN_RED, 0)
+time.sleep(1)
 
 if co2 > 800 and co2 < 1000:
     GPIO.output(PIN_GREEN, 1)
